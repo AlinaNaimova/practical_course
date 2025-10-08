560f9807153952983d94fcea99b95f2cc39834ad
Alina N <my_work_space@bk.ru>
2025-10-08 15:22:31 +0300

    create CustomLinkedList

--- src/main/java/by/internship/practical_course/utils/Node.java
+++ src/main/java/by/internship/practical_course/utils/Node.java
@@ -1,9 +1,40 @@
 package by.internship.practical_course.utils;
 
 public class Node <E> {
-	private E value;
-	private Node next;
-	public Node(E value) {
-		this.value = value;
+	private final E element;
+	private Node<E> prev;
+	private Node<E> next;
+
+	public Node(E element) {
+		this.element = element;
+		this.prev = null;
+		this.next = null;
+	}
+
+	public Node(E element, Node<E> prev, Node<E> next) {
+		this.element = element;
+		this.prev = prev;
+		this.next = next;
+	}
+
+    public E getElement() {
+        return element;
+    }
+
+
+	public Node<E> getPrev() {
+		return prev;
+	}
+
+	public Node<E> getNext() {
+		return next;
+	}
+
+	public void setPrev(Node<E> prev) {
+		this.prev = prev;
+	}
+
+	public void setNext(Node<E> next) {
+		this.next = next;
 	}
 }
